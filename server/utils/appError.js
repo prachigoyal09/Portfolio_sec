960ca99097739713eb394e917bcafb1733f64480
prachigoyal09 <101242476+prachigoyal09@users.noreply.github.com>
2025-04-16 14:43:34 +0530

    Delete server directory

--- server/utils/appError.js
+++
@@ -1,9 +0,0 @@
-class createError extends Error{
-    constructor(message, statusCode){
-        super(message);
-        this.statusCode=statusCode;
-        this.status=`${statusCode}`.startsWith('4')?"error":"fail";
-        Error.captureStackTrace(this, this.constructor);
-    }
-}
-module.exports=createError;
